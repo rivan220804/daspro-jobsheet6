0fd412527e6efeae30f9ff528f69af401e9a35b8
rivan220804 <fadillahrivan25@gmail.com>
2024-10-03 23:55:25 +0700

    Pemilihan2Percobaan123

---
+++ Pemilihan2Percobaan123.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Pemilihan2Percobaan123
+ */
+public class Pemilihan2Percobaan123 {
+
+    public static void main(String[] args) {
+        Scanner absen = new Scanner(System.in);
+        
+       
+        System.out.print("Masukkan tahun: ");
+        int tahun = absen.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+                System.out.println("Tahun Kabisat");
+        } else 
+                System.out.println("Bukan Tahun Kabisat");
+            
+        
+    }
+}
